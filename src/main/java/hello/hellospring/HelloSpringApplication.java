fbb5b00b03e41fd12ec387d90ab631e982eba558
Chun SeungUk <cheonsungwook@gmail.com>
2023-10-05 18:29:19 +0900

    entity 오류고치기

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -2,8 +2,11 @@ package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
+@EntityScan("hello.hellospring.*")
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
